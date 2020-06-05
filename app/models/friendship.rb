06bb53174edbe1b3a16a3b0c2beaea05a125e2b8
Julian Blasco <blascoblasco9@gmail.com>
2020-06-05 20:37:54 -0300

    Add callback to friendship model to create the second row when the relationship is confirmed

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  after_update do |f|
+    Friendship.create(user_id: f.friend_id, friend_id: f.user_id, status: 'accepted')
+  end
 end
